1f647ac2fe12bfda00eb88c0222f637413283789
Kien S. Juntong <129021233+Kenjun0@users.noreply.github.com>
2025-09-03 15:34:39 +0800

    Hero.jsx

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
           <div className="text-white pl-10 w-full bg-gradient-to-r from-gray-700 to-transparent md:bg-none py-8">
           <h1 className="font-bold font-sans lg:text-7xl md:text-6xl text-4xl bg-clip-text text-transparent bg-gradient-to-l from-gray-100 to-gray-400">Hello,</h1>
           <p className="md:text-4xl text-2xl font-bold uppercase text-gray-400">I'm <span className="bg-clip-text text-transparent  bg-gradient-to-l from-emerald-700 to-lime-700">Kien Juntong</span></p>
-          <p className="md:text-3xl text-2xl uppercase font-mono font-semibold bg-clip-text text-transparent bg-gradient-to-l from-gray-700 to-gray-300">'Wa ragud</p>
+          <p className="md:text-3xl text-2xl uppercase font-mono font-semibold bg-clip-text text-transparent bg-gradient-to-l from-gray-700 to-gray-300">'a web developer.</p>
           </div>
         </div>
         <img src={Image} alt="" className="h-[100vh] object-center object-cover size-[900px]" />
@@ -20,3 +20,4 @@ const Hero = () => {
 }
 
 export default Hero
+
